eacf604bfc6f7842bb71e4141b58fdb8128f99fb
Holy <holylady1304@gmail.com>
2023-06-07 19:03:02 -0400

    web MVC actualizado y nuevos archivos

---
+++ Sales.Domain/Entities/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using Sales.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Domain.Entities
+{
+    public partial class Categoria : BaseEntity
+    {
+        public int Id { get; set; }
+        public string? Descripcion { get; set; }
+    }
+}
---
+++ Sales.Infrastructure/Repositories/CategoriaRepository.cs
@@ -0,0 +1,21 @@
+﻿using Sales.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Infrastructure.Repositories
+{
+    public class CategoriaRepository
+    {
+        public List<Categoria> GetAll()
+            { 
+            return new List<Categoria>();
+            }
+        
+
+
+            
+    }
+}
